8566d9e57bfa11e27bb39d9eb74e400b958e6746
Saenaru <Nyanja230@gmail.com>
2024-12-25 00:05:41 +0300

    Update timertg.py

--- timertg.py
+++ timertg.py
@@ -3,14 +3,6 @@ from pytimeparse import parse
 from dotenv import load_dotenv
 import ptbot
 
-load_dotenv("tok.env")
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TG_CHAT_ID = os.getenv("TG_CHAT_ID")
-
-message_id_dict = {}
-
-
 def wait(chat_id, question):
     time_seconds = parse(question)
     message_id = bot.send_message(
@@ -26,17 +18,14 @@ def wait(chat_id, question):
     )
     bot.create_timer(time_seconds, choose, author_id=chat_id)
 
-
 def choose(author_id):
     bot.send_message(author_id, "Время вышло!")
-
 
 def notify_progress(secs_left, chat_id, message_id):
     total_time = message_id_dict[message_id]
     progressbar = render_progressbar(total_time, total_time - secs_left)
     message_content = "Осталось {} секунд.\n{}".format(secs_left, progressbar)
     bot.update_message(chat_id, message_id, message_content)
-
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -45,13 +34,15 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return "{0} |{1}| {2}% {3}".format(prefix, pbar, percent, suffix)
 
-
 def main():
-    global bot
-    bot = ptbot.Bot(TELEGRAM_TOKEN)
+    global bot, message_id_dict
+    load_dotenv("tok.env")
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+    tg_chat_id = os.getenv("TG_CHAT_ID")
+    message_id_dict = {}
+    bot = ptbot.Bot(telegram_token)
     bot.reply_on_message(wait)
     bot.run_bot()
-
 
 if __name__ == '__main__':
     main()
